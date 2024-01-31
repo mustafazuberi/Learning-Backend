f1f705f1b8dfa4571a43acdbd021ab3269384880
MustafaRazaZuberi <mustafazuberi986@gmail.com>
2024-01-31 15:40:04 -0800

    utils code discussion

--- Youtube/src/db/index.js
+++ Youtube/src/db/index.js
@@ -6,8 +6,7 @@ const connectDB = async() => {
     try {
         const connectionInstance = await mongoose.connect
         (`${process.env.MONGODB_URI}/${DB_NAME}`);
-       
-        console.log(`\n MongoDB Connected !! DB Host :${connectionInstance.connection.host}`)
+        console.log(`\nMongoDB Connected !! DB Host :${connectionInstance.connection.host}`)
       }
        catch (error) {
         console.log('MONGO DB Connection Error',error);

5a8e4eecd3aad91abcbb5b3496b375b21126e735
mustafazuberi <mustafazuberi986@gmail.com>
2025-04-05 20:39:40 +0500

    first appproach to connect Db

---
+++ professional-project/src/index.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants";
+import express from "express";
+
+const app = express();
+
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`);
+
+    app.on("error", (error) => {
+      console.error("ERRR:", error);
+      throw error;
+    });
+
+    app.listen(process.env.PORT, () => {
+      console.log(`Server is running on port ${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+    throw error;
+  }
+})();
